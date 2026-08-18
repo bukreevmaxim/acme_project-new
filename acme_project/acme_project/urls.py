50ef21c6ca93fb14a4ba9092b9c8131871222135
Максим Букреев <bukreev0352@gmail.com>
2026-08-18 13:05:58 +0200

    настройка страницы регистрации

--- acme_project/acme_project/urls.py
+++ acme_project/acme_project/urls.py
@@ -1,16 +1,26 @@
 # acme_project/urls.py
-# Импортируем настройки проекта
 from django.conf import settings
-# Импортируем функцию, позволяющую серверу разработки отдавать файлы
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import include, path, reverse_lazy
+from django.views.generic import CreateView
+
 
 urlpatterns = [
     path('', include('pages.urls')),
     path('admin/', admin.site.urls),
-    path('birthday/', include('birthday.urls')),
+    path(
+        'auth/registration/',
+        CreateView.as_view(
+            template_name='registration/registration_form.html',
+            form_class=UserCreationForm,
+            success_url=reverse_lazy('login'),
+        ),
+        name='registration',
+    ),
     path('auth/', include('django.contrib.auth.urls')),
+    path('birthday/', include('birthday.urls')),
 ]
 
 if settings.DEBUG:
